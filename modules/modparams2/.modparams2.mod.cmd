eb85337142b6d9ca216ff7231b003bb9261793dd
ronansena <ronansena@protonmail.com>
2022-10-21 18:17:08 -0300

    Update

---
+++ modules/modparams2/.modparams2.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/media/EXTRA_NVME_500GB_EXT4/CODE/LINUX_KERNEL/modules/modparams2/modparams2.mod := { echo  /media/EXTRA_NVME_500GB_EXT4/CODE/LINUX_KERNEL/modules/modparams2/modparams2.o;  echo; } > /media/EXTRA_NVME_500GB_EXT4/CODE/LINUX_KERNEL/modules/modparams2/modparams2.mod
